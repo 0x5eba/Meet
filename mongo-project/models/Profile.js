02cd72819db717d3d0e1a841747679774a113087
0x5eba <sbiollo@gmail.com>
2019-07-25 04:36:12 +0200

    Starting Question

--- mongo-project/models/Profile.js
+++ mongo-project/models/Profile.js
@@ -13,7 +13,10 @@ const Profile = new mongoose.Schema({
     fake_position: { x: { type: SchemaTypes.Double, default: 0 }, y: { type: SchemaTypes.Double, default: 0 } },
     online: { type: Boolean, default: false },
     lastSeen: { type: Number, default: 0 },
-    use_fake_position: { type: Boolean, default: false }
+    use_fake_position: { type: Boolean, default: false },
+
+    savedGroup: { type: [{ _id: false, name: String, idGroup: String }], default: [] },
+    savedQuestion: { type: [{ _id: false, question: String, idQuestion: String }], default: [] },
 })
 
 module.exports = mongoose.model('Profile', Profile)
