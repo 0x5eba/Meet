b7571e222c463ca3e5ed3919367b485389afd93c
0x5eba <sbiollo@gmail.com>
2019-09-10 14:50:36 +0200

    fucking froala

--- mongo-project/changeip.py
+++ mongo-project/changeip.py
@@ -1,20 +1,17 @@
 import glob, json
 paths = glob.glob("./views/*")
 
-IP = '67.205.145.243'
+IP = 'https://sebastienbiollo.com'
+PORT = 443
+replace = "http://localhost:3001"
 
 for path in paths:
     new_file = []
     with open(path, 'r') as f:
         for i in f.readlines():
-            if "localhost:3001" in i:
-                i = i.replace("localhost:3001", IP + ":80")
+            if replace in i:
+                i = i.replace(replace, IP + ":" + str(PORT))
             new_file.append(i)
-    
+
     with open(path, 'w') as f:
         f.writelines(new_file)
-
-
-data = { "ip": IP, "port": 80 }
-with open('config.json', 'w') as f:
-    json.dump(data, f)
