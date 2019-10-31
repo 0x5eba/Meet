12b389bbebc53f966069bbe8c0837db68462454f
0x5eba <sbiollo@gmail.com>
2019-10-31 09:16:56 +0100

    new cookie consent

--- models/common/config/env.config.js
+++ models/common/config/env.config.js
@@ -2,7 +2,7 @@ module.exports = {
     "jwtSecret": "ks0r1vw!eadr695QGEMP!Ns5EOcuoTNA-1eVYU4NycUvEV0d+r",
     "jwtSecret2": "mD8ayI4mQOB-u!AMWFGyL5!3EneDfi1USHnm3yDGLmB2mnp9xg",
     "jwtExpireAccessToken": 1800, // 30 min
-    "jwtExpireRefreshToken": 345600, // 4 days
+    "jwtExpireRefreshToken": 432000, // 5 days
     "permissionLevels": {
         "NORMAL_USER": 1,
         "ADMIN": 2048
